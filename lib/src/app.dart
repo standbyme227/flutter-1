f30d1225d5c560d85bc1b6bf2bc1ac7396010015
신 종민 <standbyme227@algorithmlabs.co.kr>
2019-03-28 21:23:52 +0900

    Flutter-1 c1
    코드 따라쳐보기

---
+++ lib/src/app.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+
+// class App extends StatefulWidget {
+//   createState() {
+//     return AppState();
+//   }
+// }
+// class AppState extends State<App> {
+//   int counter = 0;
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Text('$counter'),
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.clear),
+//           onPressed: () {
+//             setState(() {
+//               counter += 1;
+//             });
+//           },
+//         ),
+//         appBar: AppBar(
+//           title: Text('알고리즘랩스'),
+//         ),
+//       ),
+//     );
+//   }
+// }
